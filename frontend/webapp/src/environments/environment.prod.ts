770bb8cbbc6b08644c1fff4b351f3a68928789ed
nicolasbuscarini <nicolasbuscarini@hotmail.com>
2023-04-25 15:10:15 +0000

    remove env private

--- frontend/webapp/src/environments/environment.prod.ts
+++ frontend/webapp/src/environments/environment.prod.ts
@@ -3,6 +3,5 @@ export const environment = {
   apiUrl: 'https://144.22.138.210:5005',
   //apiUrl: 'https://host.docker.internal:5005',
   //apiUrl: 'https://localhost:443',
-  port: 5006,
-  googleMapsApiKey: process.env["GOOGLE_MAPS_API_KEY"]
+  port: 5006
 };
