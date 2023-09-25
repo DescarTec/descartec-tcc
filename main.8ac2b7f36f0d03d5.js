9f9d4a2acc32c26d35b78c7e6d5afa24e2d2495d
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2023-09-25 13:22:03 -0300

    Auto-generated commit [ci skip]

--- main.8ac2b7f36f0d03d5.js
+++ main.8ac2b7f36f0d03d5.js
@@ -1217,24 +1217,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "environment": () => (/* binding */ environment)
 /* harmony export */ });
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 const environment = {
-  production: false,
-  apiUrl: 'https://host.docker.internal:5005',
+  production: true,
+  apiUrl: 'https://144.22.138.210:5005',
+  //apiUrl: 'https://host.docker.internal:5005',
   //apiUrl: 'https://localhost:443',
   port: 5006,
   googleMapsApiKey: "AIzaSyCrqgHM60UNwwy_cw2_N7CORsw0LQAHS0Q"
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
 
 /***/ }),
 
